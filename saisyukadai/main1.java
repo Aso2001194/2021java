12e958c7c07c64f68fc5fd12880ed3da771d1c0d
Aso2001194 <83047062+Aso2001194@users.noreply.github.com>
2021-07-15 10:50:09 +0900

    Add files via upload

---
+++ saisyukadai/main1.java
@@ -0,0 +1,13 @@
+package saisyukadai;
+
+public class main1 {
+    public static void main(String[] args){
+        System.out.println("＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊");
+        System.out.println("           戦艦ゲーム");
+        System.out.println("＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊");
+
+        System.out.println("------[ターン1]--------");   
+
+        
+     }
+}
